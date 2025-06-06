04c8e9c2455d0a00d83a5596f14d0734e8d65911
Jairmandg <jair.rodrigues@mandg.com>
2025-06-06 14:30:23 +0000

    select 2

--- foundation-lesson.sql
+++ foundation-lesson.sql
@@ -1,9 +1,45 @@
-SELECT 
+SELECT
     ps.PatientId
+    , DATEADD(DAY, -14, PS.AdmittedDate) AS ReminderDate
+    , DATEDIFF(DAY, ps.AdmittedDate, ps.DischargeDate) AS LenghtOfStay
     , ps.AdmittedDate
     , ps.DischargeDate
     , ps.Hospital
-    , ps.Ward 
+    , ps.Ward
+    , ps.Tariff
 FROM PatientStay ps
 WHERE ps.Hospital IN ('Kingston', 'PRUH')
-AND ps.Ward LIKE '%Surgery'
+    -- AND ps.Ward LIKE '%Surgery'
+    AND ps.AdmittedDate >= DATEFROMPARTS(2024, 2, 27)
+    AND ps.DischargeDate BETWEEN '2024-03-03' AND '2024-03-05'
+    AND ps.Tariff > 7
+ORDER BY 
+    ps.AdmittedDate
+    , ps.DischargeDate
+
+
+SELECT
+    ps.Hospital
+    , ps.Ward
+    , COUNT(*) AS NumberOfPatients
+    , SUM(ps.Tariff) AS TotalTariff
+    , AVG(ps.Tariff) AS AvgTariff
+    , SUM(ps.Tariff)/COUNT(*) AS Average2
+FROM PatientStay ps
+--WHERE ps.Hospital in ('Oxleas', 'PRUH')
+GROUP BY ps.Hospital, ps.Ward
+HAVING COUNT (*) >3
+ORDER BY NumberOfPatients DESC
+
+SELECT *
+FROM DimHospitalBad
+SELECT
+    ps.PatientId
+    , ps.Hospital
+    , h.Hospital
+    , ps.AdmittedDate
+    , h.HospitalType
+    , h.HospitalSize
+FROM PatientStay ps LEFT JOIN DimHospitalBad h ON ps.Hospital = h.Hospital   
+
+
